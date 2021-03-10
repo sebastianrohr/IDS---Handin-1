978d198bba3bfa9855f84b9b352d5019889ce948
sebastianrohr <ssrohr@ruc.dk>
2021-03-10 15:57:16 +0100

    Knap 2 fungerer

--- npyscreenFoodAPI.py
+++ npyscreenFoodAPI.py
@@ -3,6 +3,8 @@ from npyscreen.wgtitlefield import TitleText
 import json 
 import requests
 
+#kl 15:55
+
 class App(npyscreen.NPSAppManaged):
     def onStart(self):
         #add forms to the application
@@ -15,14 +17,13 @@ class FirstForm(npyscreen.ActionFormMinimal):
         self.add(npyscreen.TitleText, w_id="coursetextfield", name="Enter your course:", rely= 5)
         self.add(npyscreen.ButtonPress, name="See ingredients", when_pressed_function=self.coursebtn_press, rely= 7)
 
+
     def ingredientsbtn_press(self):
-        ##get_recipe("potato", "olive")
         get_recipe = self.get_widget("ingredientstextfield").value
         URL = "http://www.recipepuppy.com/api/?i=" + get_recipe
-        ## url = url + get_widget.replace(' ', ',')
         getInformation = requests.get(url = URL)
         recipeInformation = getInformation.json()
-        recipeStr = " "
+        recipeStr = ""
 
         for recipe in recipeInformation['results']:
             recipeStr += "\n" + recipe['title']
@@ -30,10 +31,21 @@ class FirstForm(npyscreen.ActionFormMinimal):
         message = "You can make these different courses: " + recipeStr
         npyscreen.notify_confirm(message, title="Ingredients", wrap=True, wide=True, editw=1)
 
+
     def coursebtn_press(self):
-        message = f'Course results: {self.get_widget("coursetextfield").value}' 
+        get_ingredients = self.get_widget("coursetextfield").value
+        URL = "http://www.recipepuppy.com/api/?q=" + get_ingredients
+        getInformation = requests.get(url = URL)
+        ingredientInformation = getInformation.json()
+        
+        ingredientStr = ""
+        ingredientStr = "\n" + ingredientInformation['results'][0]['ingredients'] #tager altid det første element. Kan implementeres anderledes så den er bedere
+
+        message = "You need these ingredients for the course '" + ingredientInformation['results'][0]['title'] + "':" + ingredientStr
         npyscreen.notify_confirm(message, title="Courses", wrap=True, wide=True, editw=1)
         
+
+
     def on_ok(self):
         self.parentApp.switchForm(None)
       
