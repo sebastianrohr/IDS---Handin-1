a5ea870de4f02330cc4234b29b10de38098a07f2
Max de Visser <m.madv96@gmail.com>
2021-04-24 12:00:51 +0200

    update
    fixede små ting

--- FoodAPI.py
+++ FoodAPI.py
@@ -6,7 +6,7 @@ import random
 import pyttsx3
 import logging as log
 
-log.basicConfig(filename='Kopi.log', filemode= 'w' , encoding='utf-8', level=log.DEBUG, format='%(asctime)s %(message)s') 
+log.basicConfig(filename='Copy.log', filemode= 'w' , encoding='utf-8', level=log.INFO, format='%(asctime)s %(message)s') 
 
 class App(npyscreen.NPSAppManaged):
     def onStart(self):
@@ -14,6 +14,7 @@ class App(npyscreen.NPSAppManaged):
         
 class first_form(npyscreen.ActionForm):
     recipe_list = []
+    get_ingredients = ''
 
     def create(self):
         self.add(npyscreen.TitleText, w_id="ingredientstextfield", name="Enter your ingredients:", rely= 1)
@@ -24,14 +25,14 @@ class first_form(npyscreen.ActionForm):
         self.add(npyscreen.ButtonPress, name="Read & save log", when_pressed_function=self.logbtn_press, rely = 7, relx=39)    
 
         
-    def ingredient_information_def(self):
-        get_ingredients = self.get_widget("coursetextfield").value
-        URL = "http://www.recipepuppy.com/api/?q=" + get_ingredients
+    def ingredient_information_def(self): 
+        first_form.get_ingredients += self.get_widget("coursetextfield").value
+        URL = "http://www.recipepuppy.com/api/?q=" + first_form.get_ingredients
         get_information = requests.get(url = URL)
         ingredient_information = get_information.json()
         return ingredient_information
 
-    def format_input(self, text):
+    def format_input(self, text): # Function used to format the user-input string to the format the api uses
         new_text = ''
         for letter in text:
             if letter != ' ' and letter != '-' and letter != '/' and letter != '.':
@@ -40,7 +41,7 @@ class first_form(npyscreen.ActionForm):
                 new_text += ','
         return new_text
 
-    def ingredientsbtn_press(self): #Displays recipes based on given ingredients
+    def ingredientsbtn_press(self): # Displays recipes based on given ingredients
         get_recipe = self.format_input(self.get_widget("ingredientstextfield").value)
         URL = "http://www.recipepuppy.com/api/?i=" + get_recipe
         get_information = requests.get(url = URL)
@@ -50,30 +51,30 @@ class first_form(npyscreen.ActionForm):
            recipe_str += "\n" + recipe['title']
         npyscreen.notify_confirm(recipe_str, title="You can make these different courses", wrap=True, wide=True, editw=1)
 
-    def coursebtn_press(self): #Searches for recipes and displays ingredients
+    def coursebtn_press(self): # Searches for recipes and displays ingredients
         overall_ingredients = self.ingredient_information_def()
         ingredient_str = "\n" + overall_ingredients['results'][0]['ingredients']
         npyscreen.notify_confirm(ingredient_str, title="You need these ingredients for the course '" + overall_ingredients['results'][0]['title'] + "':", wrap=True, wide=True, editw=1)
-        if overall_ingredients['results'][0]['title'] not in first_form.recipe_list: #Avoids duplications og already saved recipes
+        if overall_ingredients['results'][0]['title'] not in first_form.recipe_list: #Avoids duplications or already saved recipes
             first_form.recipe_list.append(overall_ingredients['results'][0]['title'])
             
-    def readbtn_press(self): #Reads ingredients out loud
+    def readbtn_press(self): # Reads ingredients out loud
         overall_ingredients = self.ingredient_information_def()
         ingredient_str = "\n" + overall_ingredients['results'][0]['ingredients']
         message = "You need these ingredients for the course '" + overall_ingredients['results'][0]['title'] + "':" + ingredient_str
-        tts = pyttsx3.init()         #Initiation of speaker
-        tts.say(message)             #Speaker message
+        tts = pyttsx3.init() # Initiation of speaker
+        tts.say(message) # Speaker message
         tts.runAndWait()            
         if overall_ingredients['results'][0]['title'] not in first_form.recipe_list:
             first_form.recipe_list.append(overall_ingredients['results'][0]['title'])
 
-    def logbtn_press(self):             #A log where you can see which course you have looked at
-        message = first_form.recipe_list           #Create a String from which we can print
-        npyscreen.notify_confirm(message, title="History", wrap=True, wide=True, editw=1)       #Print the string
-        log.warning (f'Log is saved! History:{message}')
+    def logbtn_press(self): # A function that logs activity in the app
+        npyscreen.notify_confirm(first_form.recipe_list, title="History", wrap=True, wide=True, editw=1) #Print the string in the TUI
+        log_message = ' '.join(first_form.recipe_list)
+        log.info (f'Course search: {first_form.get_ingredients.upper()} -- Ingredients for course: {log_message.upper()}') # string that is logged
 
     def on_ok(self):
-        self.parentApp.setNextForm(None)
+        self.parentApp.setNextForm(None) 
 
 app = App()
 app.run()
